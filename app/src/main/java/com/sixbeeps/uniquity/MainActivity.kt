273e075ce0cc85d302bfd18573dc7a4659644b48
Brandon Lee <brandononlyu@gmail.com>
2025-08-24 00:00:54 -0400

    Update to AGP 8.12.1

--- app/src/main/java/com/sixbeeps/uniquity/MainActivity.kt
+++ app/src/main/java/com/sixbeeps/uniquity/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.sixbeeps.uniquity
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import com.sixbeeps.uniquity.ui.theme.UniquityTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,9 +34,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             UniquityTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MySimpleTextField(modifier = Modifier.padding(innerPadding))
+                    MainAppLayout(modifier = Modifier.padding(innerPadding))
                 }
             }
+        }
+    }
+
+    @Composable
+    fun MainAppLayout(modifier: Modifier = Modifier) {
+        Column(
+            modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(Dp(10f), Alignment.CenterVertically),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            MySimpleTextField()
+            Button(
+                { startActivity(Intent(Settings.ACTION_INPUT_METHOD_SETTINGS)) },
+                content = {
+                    Row {
+                        Text("Open Keyboard Settings")
+                    }
+                }
+            )
         }
     }
 
@@ -40,24 +64,18 @@ class MainActivity : ComponentActivity() {
         // State to hold the text input
         var text by remember { mutableStateOf("") }
 
-        Column(
-            modifier = modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            OutlinedTextField(
-                value = text,
-                onValueChange = { newText -> text = newText },
-                label = { Text("Enter text") }
-            )
-        }
+        OutlinedTextField(
+            value = text,
+            onValueChange = { newText -> text = newText },
+            label = { Text("Enter text") }
+        )
     }
 
     @Preview(showBackground = true)
     @Composable
     fun DefaultPreview() {
         UniquityTheme {
-            MySimpleTextField()
+            MainAppLayout()
         }
     }
 }
